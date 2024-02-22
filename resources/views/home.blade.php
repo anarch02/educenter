eacc628632c65762a78ba3fa8f17143f748c02e2
Mirjalol <mrjianarchist@gmail.com>
2024-02-22 21:39:18 +0500

    small changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,10 @@
 @section('title', $title)
 
 @section('js')
-    <!-- Sticky js -->
-    <script src="{{ asset('assets/assets/js/sticky.js') }}"></script>
 
     <!-- CHART-CIRCLE JS-->
     <script src="{{ asset('assets/js/circle-progress.min.js') }}"></script>
 
-    <!-- PIETY CHART JS-->
-    <script src="{{ asset('assets/plugins/peitychart/jquery.peity.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/peitychart/peitychart.init.js') }}"></script>
-
-    <!-- SIDEBAR JS -->
-    <script src="{{ asset('assets/plugins/sidebar/sidebar.js') }}"></script>
 
     <!-- Perfect SCROLLBAR JS-->
     <script src="{{ asset('assets/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
@@ -26,30 +18,12 @@
     <script src="{{ asset('assets/plugins/chart/rounded-barchart.js') }}"></script>
     <script src="{{ asset('assets/plugins/chart/utils.js') }}"></script>
 
-    <!-- INTERNAL SELECT2 JS -->
-    <script src="{{ asset('assets/plugins/select2/select2.full.min.js') }}"></script>
-
-    <!-- INTERNAL Data tables js-->
-    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
-    <script src="{{ asset('assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
-
-    <!-- INTERNAL APEXCHART JS -->
-    <script src="{{ asset('assets/js/apexcharts.js') }}"></script>
-    <script src="{{ asset('assets/plugins/apexchart/irregular-data-series.js') }}"></script>
 
     <!-- INTERNAL Flot JS -->
     <script src="{{ asset('assets/plugins/flot/jquery.flot.js') }}"></script>
     <script src="{{ asset('assets/plugins/flot/jquery.flot.fillbetween.js') }}"></script>
     <script src="{{ asset('assets/plugins/flot/chart.flot.sampledata.js') }}"></script>
     <script src="{{ asset('assets/plugins/flot/dashboard.sampledata.js') }}"></script>
-
-    <!-- INTERNAL Vector js -->
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-
-    <!-- SIDE-MENU JS-->
-    <script src="{{ asset('assets/plugins/sidemenu/sidemenu.js') }}"></script>
 
     <!-- TypeHead js -->
     <script src="{{ asset('assets/plugins/bootstrap5-typehead/autocomplete.js') }}"></script>
